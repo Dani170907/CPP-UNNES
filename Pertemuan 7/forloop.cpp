b7c9846c4e11db671732e73c30ea13c9828a0530
Dani170907 <daniramadhan170907@gmail.com>
2025-10-02 18:24:27 +0700

    update

---
+++ Pertemuan 7/forloop.cpp
@@ -0,0 +1,78 @@
+// looping 
+#include <iostream>
+using namespace std;
+
+int main () {
+
+   // cout << "Segitiga Bintang" << endl;
+   // for (int i = 1; i <= 5; i++){
+   //    for (int j = 1; j <= i; j++){
+   //       cout << "* ";
+   //    }
+   //    cout << endl;
+   // }
+
+
+   // int baris, kolom;
+   // cout << "Masukkan baris" << endl;
+   // cin >> baris;
+   // cout << "Masukkan kolom" << endl;
+   // cin >> kolom;
+
+   // for (int i = 1; i <= baris; i++) {        
+   //    for (int j = 1; j <= kolom; j++) {   
+   //       cout << "* ";
+   //    }
+   //    cout << endl;
+   // }
+
+
+   // cout << "Tabel Perkalian 3x3:" << endl;
+
+   // for (int i = 1; i <= 3; i++) // baris 
+   // {
+   //    for (int j = 1; j <= 3; j++)
+   //    {
+   //       cout << i * j << "\t";
+   //    }
+   //    cout << endl;
+   // }
+   
+   // Latihan Soal 3
+   // for (int i = 1; i <= 10 ; i++)
+   // {
+   //    cout << "5 X " << i << " = " << 5 * i;
+   //    cout << endl;
+   // }
+
+   // Latihan Soal 6
+   // cout << "6! = ";
+   // int hasil = 1;
+   // for (int i = 6; i >= 1; i--)
+   //    {
+   //    hasil = hasil * i;
+   //    cout << i;
+
+   //    if (i == 1)
+   //    {
+   //       cout << " = ";
+   //    } else {
+   //       cout << " x ";
+   //    }
+   // }
+   // cout << hasil;
+
+   // Latihan Soal 9
+   for (int i = 1; i <= 4; i++)
+   {
+      for (int j = 0; j <= 7; j++)
+      {
+         cout << j;
+      }
+      
+      cout << i << endl;
+   }
+   
+   
+
+}
